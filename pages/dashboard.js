0855232140727263aafa822adb522740789c0a14
hemangjagani <hemangjagani@gmail.com>
2022-01-06 23:37:54 +0530

    camera updated

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -5,8 +5,6 @@ const QrReader = dynamic(() => import("react-qr-reader"), { ssr: false });
 
 const DashBoard = () => {
   const [result, setResult] = useState("No Result");
-  const [selected, setSelected] = useState("environment");
-
   const handleScan = (data) => {
     if (data) {
       setResult(data);
@@ -17,12 +15,7 @@ const DashBoard = () => {
   };
   return (
     <div style={{ width: "600px", height: "600px" }}>
-      <select onChange={(e) => setSelected(e.target.value)}>
-        <option value={"environment"}>Back Camera</option>
-        <option value={"user"}>Front Camera</option>
-      </select>
       <QrReader
-        facingMode={selected}
         delay={300}
         onError={handleError}
         onScan={handleScan}
